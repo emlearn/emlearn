c87061b4a92ce59f3da5a7bb687b8383330297de
Jon Nordby <jononor@gmail.com>
2018-03-24 20:26:19 +0100

    arduino: Update code to take prediction tasks on serial

--- test/arduinobench/arduinobench.ino
+++ test/arduinobench/arduinobench.ino
@@ -1,35 +1,92 @@
 
 #include "emtrees.h"
 #include "emtrees_bench.h"
-#include "mytree.h"
+#include "digits.h"
 
-const int32_t n_rows = 5;
-const int32_t n_features = 61;
-const int n_repetitions = 500;
-EmtreesValue values[n_rows*n_features];
-Emtrees *trees = &myclassifier;
+const int32_t n_features = 64;
+const int32_t n_repetitions = 100000;
+
+const int32_t bytes_per_number = 50; // 32bit integer, plus separator. But better to have too much
+const int32_t buffer_length = n_features*bytes_per_number;
+char receive_buffer[buffer_length] = {0,};
+int32_t receive_idx = 0;
 
 void setup() {
   Serial.begin(115200); 
-  Serial.println("running fill");
-  emtrees_bench_fill(values, n_rows, n_features);
-  Serial.println("fill done");
 }
 
 void loop() {
+  EmtreesValue values[n_features];
+  int32_t request;
 
-  //Serial.print("%d trees, %d nodes\n", trees->n_trees, trees->n_nodes);
-  //Serial.printf("%d repetitions of %d rows with %d features\n",
-  //          n_repetitions, n_rows, n_features);
+  while (Serial.available() > 0) {
 
-  const long pre = millis();
-  const int32_t s = emtrees_bench_run(trees, values, n_rows, n_features, n_repetitions);
-  const long post = millis();
-  const long time_taken = post - pre; 
-  const long n_classifications = n_rows * n_repetitions;    
+    const char ch = Serial.read();
+    receive_buffer[receive_idx++] = ch;
 
-  Serial.print(time_taken); Serial.println("milliseconds");
-  Serial.print(n_classifications / time_taken); Serial.println("classifications/msec");
-  delay(1000);
-  //printf("class sum: %d\n", s);
+    if (receive_idx >= buffer_length-1) {
+        receive_idx = 0;
+        memset(receive_buffer, buffer_length, 0);
+        Serial.println("Error, buffer overflow");
+    }
+    
+    if (ch == '\n') {
+      
+        // Parse the values to use for prediction
+        int field_no = 0;
+        char seps[] = ",;";
+        char *token = strtok(receive_buffer, seps);
+        while (token != NULL)
+        {
+            long value;
+            sscanf(token, "%ld", &value);
+            if (field_no == 0) {
+              request = value;
+            } else {
+              values[field_no-1] = value; 
+            }
+            field_no++;
+            token = strtok(NULL, seps);
+        }
+
+        if (field_no-1 != n_features) {
+            Serial.print("Error, wrong number of features: "); Serial.println(field_no-1);
+        }
+        
+        receive_idx = 0;
+        memset(receive_buffer, buffer_length, 0);
+
+        // Do predictions
+        int32_t sum = 0;
+        const long pre = millis();
+        int32_t prediction = -999;
+        for (int32_t i=0; i<n_repetitions; i++) {
+          const int32_t p = digits_predict(values, n_features);
+          if (prediction != -999 && p != prediction) {
+              // consistency check, should always be same
+              prediction = -2;
+              break;
+          }
+          //Serial.print("cl: "); Serial.println(s);
+          sum += p;
+          prediction = p;
+        }
+        const long post = millis();
+        const long time_taken = post - pre;     
+
+        // Send back on parseable format
+        Serial.print(request);
+        Serial.print(";");
+        Serial.print(time_taken);
+        Serial.print(";");
+        Serial.print(prediction);
+        Serial.print(";");
+        Serial.print(sum);
+        for (int i=0; i<n_features; i++) {
+          Serial.print(";");
+          Serial.print(values[i]);
+        }
+        Serial.print("\n");
+    }
+  }
 }
