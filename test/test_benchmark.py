a221c60c6f2d14600a7d052411b32b95f15883f1
Mark Cooke <micooke@users.noreply.github.com>
2019-11-19 21:45:13 +1030

    bugfix : missing variable output_filename

--- test/test_benchmark.py
+++ test/test_benchmark.py
@@ -9,13 +9,13 @@ import sys
 from distutils.ccompiler import new_compiler
 
 def test_bench_melspec():
-    testdir = os.path.dirname(__file__)
-    code = os.path.join(testdir, 'bench.c')
-    prog = os.path.join(testdir, 'bench')
-
     # create a new compiler object
     cc = new_compiler()
-    prog = cc.executable_filename('bench')
+    output_filename = cc.executable_filename('bench')
+    
+    testdir = os.path.dirname(__file__)
+    code = os.path.join(testdir, 'bench.c')
+    prog = os.path.join(testdir, output_filename)
     include_dirs=["emlearn"]
 
     if sys.platform.startswith('win'): # Windows
@@ -24,7 +24,7 @@ def test_bench_melspec():
         cc_args = ["-O3","-g","-fno-omit-frame-pointer","-Wall","-Werror"]
 
     objects = cc.compile([code], output_dir=testdir, include_dirs=include_dirs,
-        debug=1, )
+        debug=1)
     cc.link("executable", objects, output_filename=output_filename, 
         debug=1, output_dir=testdir)
     out = subprocess.check_output([prog]).decode('utf-8')
