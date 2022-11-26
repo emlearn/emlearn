263b2a29e472d42ebdda6d11b3be4928851b3993
Jon Nordby <jononor@gmail.com>
2022-11-26 14:09:03 +0100

    evaluate: Initial submodule with tree ensemble metrics and Pareto calculation

---
+++ emlearn/evaluate/pareto.py
@@ -0,0 +1,106 @@
+
+import numpy
+
+def is_pareto_efficient_simple(costs):
+    """
+    Find the pareto-efficient points (smaller is better)
+    
+    :param costs: An (n_points, n_costs) array
+    :return: A (n_points, ) boolean array, indicating whether each point is Pareto efficient
+    
+    From https://stackoverflow.com/a/40239615/1967571
+    Fairly fast for many datapoints, less fast for many costs, somewhat readable
+    """
+    is_efficient = numpy.ones(costs.shape[0], dtype = bool)
+    for i, c in enumerate(costs):
+        if is_efficient[i]:
+            is_efficient[is_efficient] = numpy.any(costs[is_efficient] < c, axis=1)  # Keep any point with a lower cost
+            is_efficient[i] = True  # And keep self
+    return is_efficient
+
+
+def find_pareto_front(df,
+    cost_metric='mean_test_compute',
+    performance_metric='mean_test_accuracy',
+    higher_is_better=True,
+    min_performance=None):
+    
+
+    # prevnt mutating input    
+    df = df.copy()
+
+    # flip to negative-is-better
+    if higher_is_better:
+        df['mean_test_score'] = -df[performance_metric]
+    else:
+        df['mean_test_score'] = df[performance_metric]
+
+    pp = is_pareto_efficient_simple(df[['mean_test_score', cost_metric]].values)
+    
+    if min_performance is not None:
+        if higher_is_better:
+            pp = (pp & (df[performance_metric] >= min_performance))
+        else:
+            pp = (pp & (df[performance_metric] <= min_performance))
+    
+    return df[pp]
+
+def plot_pareto_front(results,
+                      pareto_cut=None,
+                      plot_other=True,
+                      plot_pareto=True,
+                      hue=None,
+                      pareto_alpha=0.8,
+                      other_alpha=0.3,
+                      pareto_global=False,
+                      s=100,
+                      pareto_s=5,
+                      height=8,
+                      aspect=1,
+                      cost_metric='mean_test_compute',
+                      performance_metric='mean_test_accuracy',
+                      size_metric='mean_test_size'):
+
+    import seaborn
+
+    pf = find_pareto_front(results, min_performance=pareto_cut)
+    pf = pf.sort_values(cost_metric)
+
+    g = seaborn.FacetGrid(results, hue=hue, height=height, aspect=aspect)
+
+    # plot all data
+    if plot_other:
+        g.map_dataframe(seaborn.scatterplot,
+                        x=cost_metric,
+                        y=performance_metric,
+                        size=size_metric,
+                        legend=False,
+                        s=pareto_s*s,
+                        alpha=other_alpha,
+                        #hue=hue,
+       )
+
+    def _plot_front(color, label):
+        if pareto_global:
+            sub = pf.copy()
+        else:        
+            sub = pf[pf[hue] == label]        
+
+        seaborn.lineplot(
+            data=sub,
+            x=cost_metric,
+            y=performance_metric,
+            #color=color,
+            label=label,
+            alpha=pareto_alpha,
+            legend=True,
+        )
+
+    # plot the data along Pareto front 
+    if plot_pareto:
+        g.map(_plot_front)
+
+    return g
+
+
+
---
+++ emlearn/evaluate/trees.py
@@ -0,0 +1,67 @@
+
+from ..convert import convert as convert_model
+
+import numpy
+
+def get_tree_estimators(estimator):
+    """
+    Get the DecisionTree instances from ensembles or single-tree models
+    """
+    if hasattr(estimator, 'estimators_'):
+        trees = [ e for e in estimator.estimators_]
+    else:
+        trees = [ estimator ]
+    return trees
+        
+def model_size_nodes(model, a=None, b=None):    
+    em = convert_model(model)
+    
+    nodes, roots = em.forest_
+    return len(nodes)
+
+def model_size_bytes(model, a=None, b=None, node_size=None):
+    # EmlTreesNode consists of feature index, a threshold value, left-right child indices 
+    if node_size is None:
+        node_size = 1+4+2+2
+
+    nodes = model_size_nodes(model)
+    bytes = nodes * node_size
+    return bytes
+
+def tree_depth_average(model, a=None, b=None):
+    trees = get_tree_estimators(model)
+    depths = [ e.tree_.max_depth for e in trees ]
+    return numpy.mean(depths)
+
+def tree_depth_difference(model, a=None, b=None):
+    """Measures how much variation there is in tree depths"""
+    trees = get_tree_estimators(model)
+    depths = [ e.tree_.max_depth for e in trees ]
+    return numpy.max(depths) - numpy.min(depths)
+
+def count_trees(model, a=None, b=None):
+    trees = get_tree_estimators(model)
+    return len(trees)
+
+def compute_cost_estimate(model, X, b=None):
+    """
+    Make an estimate of the compute cost, using the following assumptions:
+    
+    - The dataset X is representative of the typical dataset
+    - Cost is proportional to the number of decision node evaluation in a tree
+    - The cost is added across all trees in the ensemble
+
+    Under this model, the actual compute time can be computed as the estimate times a constant C,
+    representing the time a single evaluation of a decision node takes.
+    """
+    trees = get_tree_estimators(model)
+    
+    X = numpy.array(X)
+
+    total = 0.0
+    for e in trees:
+        path = e.decision_path(X)
+        t = numpy.sum(path, axis=1)
+        total += numpy.mean(t)
+    
+    return total
