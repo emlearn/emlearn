b93304a9578b7808434ed16090d06d6133384f00
Jon Nordby <jononor@gmail.com>
2023-06-13 20:55:46 +0200

    tests: Run C/Unity tests as part of pytest

--- test/test_all.c
+++ test/test_all.c
@@ -9,6 +9,19 @@
 #include "test_array.c"
 #include <unity.c>
 
+// Declare the different modules
+typedef void (*TestModuleFunction)(void);
+
+typedef struct _TestModule {
+    const char * name;
+    TestModuleFunction func;
+} TestModule;
+
+#define TEST_MODULES 1
+TestModule test_modules[TEST_MODULES] = {
+    { "array", test_eml_array },
+};
+
 void
 setUp(void)
 {
@@ -21,13 +34,41 @@ tearDown(void)
     // clean stuff up here
 }
 
-// gcc -std=c99 -o run_tests test/test_all.c -g -I./emlearn -I./test/Unity/src/ -lm && ./run_tests
-int main(void)
+int
+run_tests(const char *tests)
 {
     UNITY_BEGIN();
 
     // Run the different test modules
-    test_eml_array();
+    const char *tests_enabled = tests;
+    const char *env = getenv("EMLEARN_TEST_MODULES");
+    if (!tests_enabled) {
+        tests_enabled = env;
+    }
+
+    EML_LOG_BEGIN("run-tests");
+    EML_LOG_ADD("env", env);
+    EML_LOG_ADD("enabled", tests_enabled);
+    EML_LOG_END();
+
+    for (int i=0; i<TEST_MODULES; i++) {
+        const TestModule *mod = &test_modules[i];
+
+        bool run_test = true;
+        if (tests_enabled != NULL) {
+            run_test = strstr(mod->name, tests_enabled) != NULL;
+        }
+
+        if (run_test) {
+            mod->func();
+        }
+    }
 
     return UNITY_END();
+}
+
+// gcc -std=c99 -o run_tests test/test_all.c -g -I./emlearn -I./test/Unity/src/ -lm && ./run_tests
+int main(void)
+{
+    run_tests(NULL);
 }
