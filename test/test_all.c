c0c1b7dab880259d99251ade0a04bf868f202a8c
Jon Nordby <jononor@gmail.com>
2023-10-27 00:22:02 +0200

    tests: Add a basic C test for net

--- test/test_all.c
+++ test/test_all.c
@@ -9,6 +9,7 @@
 #include "test_array.c"
 #include "test_neighbors.c"
 #include "test_quantizer.c"
+#include "test_net.c"
 
 #include <unity.c>
 
@@ -20,11 +21,12 @@ typedef struct _TestModule {
     TestModuleFunction func;
 } TestModule;
 
-#define TEST_MODULES 3
+#define TEST_MODULES 4
 TestModule test_modules[TEST_MODULES] = {
     { "array", test_eml_array },
     { "neighbors", test_eml_neighbors },
     { "quantizer", test_eml_quantizer },
+    { "net", test_eml_net },
 };
 
 void
