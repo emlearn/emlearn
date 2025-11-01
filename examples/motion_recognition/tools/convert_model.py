72f981aec746ecbec10bb0702efd2324c13186ef
Jon Nordby <jononor@gmail.com>
2025-11-01 19:35:45 +0100

    motion: Unhardcode paths for tooling

--- examples/motion_recognition/tools/convert_model.py
+++ examples/motion_recognition/tools/convert_model.py
@@ -1,22 +1,41 @@
 
+"""Convert a scikit-learn RandomForest to a C header using emlearn"""
+
 import emlearn
-
 import pickle
+import argparse
 
-# Load from file
-with open('combined.estimator.pickle', 'rb') as f:
-    estimator = pickle.load(f)
+def parse():
+    parser = argparse.ArgumentParser(description='Convert model to C code')
+    parser.add_argument('--model', type=str, default=None,
+                       help='Input model pickle file')
+    parser.add_argument('--out', type=str, default='build/motion_model.h',
+                       help='Output header file path')
+    parser.add_argument('--name', type=str, default='motion_model',
+                       help='Model name in generated code')
+    parser.add_argument('--dtype', type=str, default='float',
+                       choices=['float', 'double', 'int8', 'int16'],
+                       help='Data type for model parameters')
+    parser.add_argument('--method', type=str, default='inline',
+                       choices=['inline', 'loadable'],
+                       help='Code generation method')
+    parser.add_argument('--leaf_bits', type=int, default=4,
+                       help='Number of bits for leaf values')
+    return parser.parse_args()
 
-
-    print(estimator)
-
-    c_model = emlearn.convert(estimator,
-        dtype='float',
-        method='inline',
-        leaf_bits=4,
-    )
-
-    out = 'build/motion_model.h'
-    c_model.save(file=out, name='motion_model')
-
-    print('Wrote', out)
+if __name__ == '__main__':
+    args = parse()
+    
+    # Load from file
+    with open(args.model, 'rb') as f:
+        estimator = pickle.load(f)
+        print(estimator)
+        
+        c_model = emlearn.convert(estimator,
+            dtype=args.dtype,
+            method=args.method,
+            leaf_bits=args.leaf_bits,
+        )
+        
+        c_model.save(file=args.out, name=args.name)
+        print('Wrote', args.out)
--- examples/motion_recognition/tools/gravity_lowpass.py
+++ examples/motion_recognition/tools/gravity_lowpass.py
@@ -1,9 +1,9 @@
 
+import argparse
+
 import pandas
 import numpy
-
 import emlearn
-
 from scipy.signal import iirfilter, sosfiltfilt
 
 def create_lowpass(samplerate, cutoff=0.5, order=4):
@@ -16,27 +16,40 @@ def create_lowpass(samplerate, cutoff=0.5, order=4):
 
     return sos
 
+def parse():
+    parser = argparse.ArgumentParser(description='Generate lowpass filter')
+    parser.add_argument('--out', type=str, default='build/gravity_filter.h',
+                       help='Output file path')
+    parser.add_argument('--format', type=str, default='header', 
+                       choices=['header', 'npy'],
+                       help='Output format')
+    parser.add_argument('--name', type=str, default='gravity_lowpass',
+                       help='Variable name prefix')
+    parser.add_argument('--samplerate', type=float, default=50,
+                       help='Sample rate in Hz')
+    parser.add_argument('--cutoff', type=float, default=0.5,
+                       help='Cutoff frequency in Hz')
+    parser.add_argument('--order', type=int, default=4,
+                       help='Filter order')
+    return parser.parse_args()
+
 if __name__ == '__main__':
-
-    sos = create_lowpass(samplerate=50)
-
-    out = 'build/gravity_lowpass.npy'
+    args = parse()
+    
+    sos = create_lowpass(samplerate=args.samplerate, 
+                        cutoff=args.cutoff, 
+                        order=args.order)
     flat = sos.flatten()
-    numpy.save(out, sos)
-    #print(sos.shape, sos)
-    print(flat.shape, flat)
-
-
-    name = 'gravity_lowpass'
-    length = len(flat)
-    code = '// Generated using lowpass.py\n\n' + \
-        f'#define {name}_length {length}\n' + \
-        emlearn.cgen.array_declare(name=f'{name}_values', values=flat) + '\n'
-
-    header = 'build/gravity_filter.h'
-
-    with open(header, 'w') as f:
-        f.write(code)
-
-    print('Wrote', header)
-
+    
+    if args.format == 'npy':
+        numpy.save(args.out, sos)
+        print('Wrote', args.out)
+    else:
+        length = len(flat)
+        code = '// Generated using lowpass.py\n\n' + \
+            f'#define {args.name}_length {length}\n' + \
+            emlearn.cgen.array_declare(name=f'{args.name}_values', values=flat) + '\n'
+        
+        with open(args.out, 'w') as f:
+            f.write(code)
+        print('Wrote', args.out)
